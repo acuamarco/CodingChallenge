1876bc8e6c9a00add130039007f6038b4eb7038b
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-24 08:35:11 -0600

    Adding initial web test and model

---
+++ CodingChallenge.Web.Tests/Models/ProjectTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge.Web.Models;
+
+namespace CodingChallenge.Web.Tests.Models
+{
+    [TestClass]
+    public class ProjectTests
+    {
+        [TestMethod]
+        public void TimeToStartInPastTest()
+        {
+            var project = new Project() { StartDate = DateTime.Now.AddDays(-5) };
+
+            Assert.AreEqual("Started", project.TimeToStart);
+        }
+
+        [TestMethod]
+        public void TimeToStartSameDayTest()
+        {
+            var project = new Project() { StartDate = DateTime.Now };
+
+            Assert.AreEqual("0", project.TimeToStart);
+        }
+
+        [TestMethod]
+        public void TimeToStartInFutureTest()
+        {
+            var project = new Project() { StartDate = DateTime.Now.AddDays(3) };
+
+            Assert.AreEqual("3", project.TimeToStart);
+        }
+
+        [TestMethod]
+        public void IsActiveTest()
+        {
+            var project = new Project() { IsActive = true };
+
+            Assert.AreEqual("Active", project.Status);
+        }
+
+        [TestMethod]
+        public void IsInactiveTest()
+        {
+            var project = new Project() { IsActive = false };
+
+            Assert.AreEqual("Inactive", project.Status);
+        }
+    }
+}
---
+++ CodingChallenge.Web/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodingChallenge.Web.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public string TimeToStart
+        {
+            get
+            {
+                if (StartDate.Date >= DateTime.Now.Date) {
+                    return StartDate.Subtract(DateTime.Now).Days.ToString();
+                }
+                else
+                {
+                    return "Started";
+                }
+            }
+        }
+        public DateTime EndDate { get; set; }
+        public int Credits { get; set; }
+        public bool IsActive { get; set; }
+        public string Status
+        {
+            get
+            {
+                return IsActive ? "Active" : "Inactive";
+            }
+        }
+    }
+}
