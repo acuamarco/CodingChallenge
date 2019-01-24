1679496027f122185277750426db9dd154aec2ba
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-24 09:04:42 -0600

    Adding web user class and test

---
+++ CodingChallenge.Web.Tests/Models/UserTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge.Web.Models;
+
+namespace CodingChallenge.Web.Tests.Models
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void FirstNameNullShouldReturnEmptyStringTest()
+        {
+            var user = new User() { FirstName = null };
+
+            Assert.AreEqual(string.Empty, user.FirstName);
+        }
+
+        [TestMethod]
+        public void FirstNameShouldReturnTrimmedTest()
+        {
+            var user = new User() { FirstName = " first " };
+
+            Assert.AreEqual("first", user.FirstName);
+        }
+
+        [TestMethod]
+        public void LastNameNullShouldReturnEmptyStringTest()
+        {
+            var user = new User() { LastName = null };
+
+            Assert.AreEqual(string.Empty, user.LastName);
+        }
+
+        [TestMethod]
+        public void LastNameShouldReturnTrimmedTest()
+        {
+            var user = new User() { LastName = " last " };
+
+            Assert.AreEqual("last", user.LastName);
+        }
+
+        [TestMethod]
+        public void NameShouldConcatenateFirstAndLastTest()
+        {
+            var user = new User() {FirstName = " first ", LastName = " last "};
+
+            Assert.AreEqual("first last", user.Name);
+        }
+    }
+}
---
+++ CodingChallenge.Web/Models/User.cs
@@ -0,0 +1,39 @@
+﻿namespace CodingChallenge.Web.Models
+{
+    public class User
+    {
+        private string firstName;
+        private string lastName;
+
+        public int Id { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return string.IsNullOrEmpty(firstName) ? "" : firstName.Trim();
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName {
+            get
+            {
+                return string.IsNullOrEmpty(lastName) ? "" : lastName.Trim();
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Name {
+            get
+            {
+                return string.Concat(FirstName, " ", LastName).Trim();
+            }
+        }
+    }
+}
