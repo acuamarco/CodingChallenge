50bad40dead3d5790c4f5f65bd408ec9689bd004
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-25 06:21:29 -0600

    Removing boilerplate and integrating Angular into the application Index

--- CodingChallenge.Web/Controllers/HomeController.cs
+++ CodingChallenge.Web/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CodingChallenge.Web.Controllers
@@ -10,20 +6,6 @@ namespace CodingChallenge.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
