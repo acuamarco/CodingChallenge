0c022769c027c625635475177ecd9a2d90e19dee
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-26 09:43:39 -0600

    Adding mock data into the API for users and projects

--- CodingChallenge.Web/App_Start/WebApiConfig.cs
+++ CodingChallenge.Web/App_Start/WebApiConfig.cs
@@ -1,30 +1,30 @@
-﻿using System;
+using System;
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
 
 namespace CodingChallenge.Web
 {
-    public static class WebApiConfig
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+      // Web API configuration and services
+      GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+          new System.Net.Http.Formatting.RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json")
+      );
 
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
-                new System.Net.Http.Formatting.RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json")
-            );
+      // Use camel case for JSON data
+      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-            // Use camel case for JSON data
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      // Web API routes
+      config.MapHttpAttributeRoutes();
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
+      // Convention-based routing.
+      config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-        }
     }
+  }
 }
--- CodingChallenge.Web/Controllers/UserController.cs
+++ CodingChallenge.Web/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -6,22 +7,38 @@ using CodingChallenge.Web.Models;
 
 namespace CodingChallenge.Web.Controllers
 {
-    public class UserController : ApiController
+  public class UserController : ApiController
+  {
+    [ResponseType(typeof(List<User>))]
+    public async Task<IHttpActionResult> Get()
     {
-        [ResponseType(typeof(List<User>))]
-        public async Task<IHttpActionResult> Get()
-        {
-            var users = await GetUsers();
-            return Ok(users);
-        }
+      var users = await _GetUsers();
+      return Ok(users);
+    }
 
-        private async Task<IEnumerable<User>> GetUsers()
-        {
-            return await Task.FromResult(new List<User>()
+    [ResponseType(typeof(List<Project>))]
+    [Route("api/user/{userId}/projects")]
+    public async Task<IHttpActionResult> GetProjects(int userId)
+    {
+      var projects = await _GetProjects(userId);
+      return Ok(projects);
+    }
+
+    private async Task<IEnumerable<User>> _GetUsers()
+    {
+      return await Task.FromResult(new List<User>()
             {
                 new User() { Id = 1, FirstName = "User", LastName = "1" },
                 new User() { Id = 1, FirstName = "User", LastName = "2" }
             });
-        }
     }
+
+    private async Task<IEnumerable<Project>> _GetProjects(int userId)
+    {
+      return await Task.FromResult(new List<Project>()
+      {
+        new Project() {Id = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1), Credits = 1, IsActive = true }
+      });
+    }
+  }
 }
