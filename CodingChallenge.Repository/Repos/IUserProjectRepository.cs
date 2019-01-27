e521a3c0f75771015d679fa4ff668c28c0f0cce7
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-26 23:44:03 -0600

    Fixed repo type

--- CodingChallenge.Repository/Repos/IUserProjectRepository.cs
+++ CodingChallenge.Repository/Repos/IUserProjectRepository.cs
@@ -3,7 +3,7 @@ using CodingChallenge.Repository.Model;
 
 namespace CodingChallenge.Repository.Repos
 {
-    public interface IUserProjectRepository : IGenericRepository<Project>
+    public interface IUserProjectRepository : IGenericRepository<UserProject>
     {
         IQueryable<UserProject> GetByUserId(int userId);
     }
