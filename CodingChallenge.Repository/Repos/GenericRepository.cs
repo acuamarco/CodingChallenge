cddd7040c2b6d4191eb9f132722e07b58282978b
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-26 21:07:56 -0600

    Base generic repository interface and class

---
+++ CodingChallenge.Repository/Repos/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Repository
+{
+
+    public abstract class GenericRepository<T> : IGenericRepository<T>
+        where T : class, new()
+    {
+        protected CodingChallengeContext DbContext { get; set; }
+
+        public IQueryable<T> Query()
+        {
+            return DbContext.Set<T>().AsQueryable();
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            DbContext.Set<T>().Add(entity);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            DbContext.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            await DbContext.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ CodingChallenge.Repository/Repos/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Repository
+{
+    public interface IGenericRepository<T>
+    {
+        IQueryable<T> Query();
+
+        Task InsertAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
