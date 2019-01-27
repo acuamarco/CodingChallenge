55498e6948f6d2d56392d31bdcd2dee8015f46d3
Marco Rodriguez <marco@rodriguezcoto.com>
2019-01-26 18:00:31 -0600

    Creating data layer proyect

---
+++ CodingChallenge.Repository/CodingChallengeContext.cs
@@ -0,0 +1,40 @@
+namespace CodingChallenge.Repository
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class CodingChallengeContext : DbContext
+    {
+        public CodingChallengeContext()
+            : base("name=cc_db")
+        {
+        }
+
+        public virtual DbSet<Project> Projects { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserProject> UserProjects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.UserProjects)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.FirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.LastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.UserProjects)
+                .WithRequired(e => e.User)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
